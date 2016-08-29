95cff1593723a9e3837a597be4aac60f471cc35d
ywp <ywp@DESKTOP-JVCRBUU>
2016-08-29 19:51:29 +0800

    开始邮件发送系统

---
+++ src/com/doctor/nyqx/commons/util/MailUtil.java
@@ -0,0 +1,9 @@
+package com.doctor.nyqx.commons.util;
+
+public class MailUtil {
+	private static final String PORT = "25";
+	private static final String HOST = "smtp.163.com";
+	private static final String USER_NAME = "13277415918@163.com";
+	private static final String PASSWORD = "amnqvjsiqaexntvb";
+	private static final String FROM = "13277415918@163.com";
+}
